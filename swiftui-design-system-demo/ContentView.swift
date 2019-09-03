5ea2a95606a565c54456ebcb158f1d44be73146f
vince19972 <vince19972@gmail.com>
2019-09-03 12:22:56 -0400

    :tada: initial commit

---
+++ swiftui-design-system-demo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  swiftui-design-system-demo
+//
+//  Created by 邵名浦 on 2019/9/3.
+//  Copyright © 2019 vinceshao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
