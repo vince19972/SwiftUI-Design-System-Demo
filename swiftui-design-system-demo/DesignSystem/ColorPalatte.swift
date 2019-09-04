0a88ec4ce721d91e7669bf7d2d573a0f623b58b1
vince19972 <vince19972@gmail.com>
2019-09-04 10:07:48 -0400

    :lipstick: add color sets (light, dark, button…)

--- swiftui-design-system-demo/DesignSystem/ColorPalatte.swift
+++ swiftui-design-system-demo/DesignSystem/ColorPalatte.swift
@@ -14,12 +14,17 @@ import SwiftUI
 
 /// 1. Level 1 materials
 struct BaseColor {
+    /// dynamic color sets (with dark and light mode)
     let contrastPrimary = Color("contrastPrimary")
     let contrastSecondary = Color("contrastSecondary")
     let themePrimary = Color("themePrimary")
     let themeSecondary = Color("themeSecondary")
     let brandPrimary = Color("brandPrimary")
     let brandSecondary = Color("brandSecondary")
+    
+    /// staic color sets (not updating along with color mode)
+    let darkPrimary = Color("darkPrimary")
+    let lightPrimary = Color("lightPrimary")
     let gray = Color("gray")
 }
 
@@ -31,17 +36,39 @@ struct TokenColor {
     let inactive: Color!
     
     let textDefault: Color!
+    let textTheme: Color!
+    let textNote: Color!
     let textHighlight: Color!
+    let textLight: Color!
+    
+    let buttonTheme: Color!
+    let buttonContrast: Color!
+    let buttonHighlight: Color!
+    
     let backgroundDefault: Color!
+    let backgroundTheme: Color!
     
     init() {
+        /// themePrimary
+        self.textTheme = baseColor.themePrimary
+        self.buttonTheme = baseColor.themePrimary
+        self.backgroundTheme = baseColor.themePrimary
+        
+        /// themeSecondary
+        self.textNote = baseColor.themeSecondary
+        
         /// contrastPrimary
+        self.buttonContrast = baseColor.contrastPrimary
         self.textDefault = baseColor.contrastPrimary
         self.backgroundDefault = baseColor.contrastPrimary
         
         /// brand
         self.highlight = baseColor.brandPrimary
+        self.buttonHighlight = baseColor.brandPrimary
         self.textHighlight = baseColor.brandPrimary
+        
+        /// lightPrimary
+        self.textLight = baseColor.lightPrimary
         
         /// gray
         self.inactive = baseColor.gray
