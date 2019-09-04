e930368580cf1eb149622ea4282c6313cbc03611
vince19972 <vince19972@gmail.com>
2019-09-04 10:08:46 -0400

    :sparkles: add AliasHelper file

---
+++ swiftui-design-system-demo/Helper/AliasHelper.swift
@@ -0,0 +1,14 @@
+//
+//  AliasHelper.swift
+//  swiftui-design-system-demo
+//
+//  Created by 邵名浦 on 2019/9/4.
+//  Copyright © 2019 vinceshao. All rights reserved.
+//
+
+import SwiftUI
+
+struct StyleAlias {
+    typealias BorderStyle = (color:Color, width:CGFloat, cornerRadius:CGFloat)
+}
+
